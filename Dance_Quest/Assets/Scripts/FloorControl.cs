6c5570fd3bf64f09203c9e15cadb847279e8c5ab
Paul <iamtheodo@gmail.com>
2019-11-12 11:29:11 +1100

    Individual Floor Control Added

--- Dance_Quest/Assets/Scripts/FloorControl.cs
+++ Dance_Quest/Assets/Scripts/FloorControl.cs
@@ -6,28 +6,27 @@ namespace VirtualNowQuest
     /// <summary>
     /// This class is responsible for enabling user to toggle the "dance floor" on / off
     /// </summary>
-    public class FloorControl : MonoBehaviourPun
+    public class FloorControl : MonoBehaviour
     {
-        private MeshRenderer _Floor;
+        public MeshRenderer floor;
         private bool _Toggle;
 
         // Start is called before the first frame update
         private void Start()
         {
-            _Floor = GameObject.FindGameObjectWithTag("Dance Floor").GetComponent<MeshRenderer>();
             _Toggle = true;
+            floor.gameObject.SetActive(true);
+            floor.gameObject.transform.SetParent(null);
+            floor.gameObject.transform.position = Vector3.zero;
         }
 
         // Update is called once per frame
         void Update()
         {
-            if (photonView.IsMine)
+            if (OVRInput.GetDown(OVRInput.Button.PrimaryThumbstick) || OVRInput.GetDown(OVRInput.Button.SecondaryThumbstick))
             {
-                if (OVRInput.GetDown(OVRInput.Button.One) || OVRInput.GetDown(OVRInput.Button.Two))
-                {
-                    _Toggle = !_Toggle;
-                    ToggleFloor();
-                }
+                _Toggle = !_Toggle;
+                ToggleFloor();
             }
         }
 
@@ -36,7 +35,7 @@ namespace VirtualNowQuest
         /// </summary>
         private void ToggleFloor()
         {
-            _Floor.enabled = _Toggle;
+            floor.enabled = _Toggle;
         }
     }
 }
